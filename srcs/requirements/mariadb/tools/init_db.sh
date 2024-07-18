47926d4b0c5f51ac592fa9af06c29d406cc7b36b
Jihyun Kim <ksk208105@gmail.com>
2024-07-18 17:22:37 +0900

    feat: mariadb setting

---
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# set shell-script
+set -e
+
+# init MariaDB and start
+mariadb --user=mysql --datadir=/var/lib/mysql --bootstrap << EOF
+    FLUSH PRIVILEGES;
+    CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+    SET PASSWORD FOR root@localhost = PASSWORD('$MYSQL_ROOT_PASSWORD');
+    GRANT ALL ON *.* TO root@localhost WITH GRANT OPTION;
+    CREATE USER IF NOT EXISTS root@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+    SET PASSWORD FOR root@'%' = PASSWORD('$MYSQL_ROOT_PASSWORD');
+    GRANT ALL ON *.* TO root@'%' WITH GRANT OPTION;
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+    CREATE USER IF NOT EXISTS $MYSQL_USER_ID@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
+    SET PASSWORD FOR $MYSQL_USER_ID@'%' = PASSWORD('$MYSQL_USER_PASSWORD');
+    GRANT ALL ON $MYSQL_DATABASE.* TO $MYSQL_USER_ID@'%' WITH GRANT OPTION;
+EOF
+
+# exit script and excute command
+# `$@` means all arguments
+exec $@
