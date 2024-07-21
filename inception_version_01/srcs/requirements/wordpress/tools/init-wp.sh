faeb9399fab7d2d1b3ae8c88e223aeab8d85204d
Jihyun Kim <ksk208105@gmail.com>
2024-07-21 14:53:45 +0900

    Fix: update version and change commands from new-version(debian)

--- inception_version_01/srcs/requirements/wordpress/tools/init-wp.sh
+++ inception_version_01/srcs/requirements/wordpress/tools/init-wp.sh
@@ -11,15 +11,23 @@ else
     rm -rf latest.tar.gz
     rm -rf wordpress
 
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+
+    # chown -R www-data:www-data .
+    # chmod -R u+w .
+
+    # sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+    # sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    # sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    # sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    # cp wp-config-sample.php wp-config.php
+
+    perl -pi -e "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+    perl -pi -e "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    perl -pi -e "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    perl -pi -e "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
     cp wp-config-sample.php wp-config.php
 
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
+
     wp core install --url=$DOMAIN_NAME \
                     --title="Hello~~ Nice to meet you~~" \
                     --admin_user=$WP_ROOT_USERNAME \
